df382f09516883d08132e929310df9096708ca6e
farhan <farhanasfar21@gmail.com>
2026-01-13 10:23:29 +0600

    Regenerate prisma migration

--- prisma/migrations/0_init/migration.sql
+++ prisma/migrations/0_init/migration.sql
@@ -1,15 +1,5 @@
--- CreateTable
-CREATE TABLE "Location" (
-    "id" SERIAL NOT NULL,
-    "city" TEXT,
-    "cityName" TEXT NOT NULL,
-    "country" TEXT,
-    "countryName" TEXT NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL,
-
-    CONSTRAINT "Location_pkey" PRIMARY KEY ("id")
-);
+-- CreateSchema
+CREATE SCHEMA IF NOT EXISTS "public";
 
 -- CreateTable
 CREATE TABLE "Airport" (
@@ -21,6 +11,20 @@ CREATE TABLE "Airport" (
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "Airport_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Attraction" (
+    "id" SERIAL NOT NULL,
+    "address" TEXT NOT NULL,
+    "city" TEXT NOT NULL,
+    "country" TEXT NOT NULL,
+    "description" TEXT,
+    "locationId" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Attraction_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
@@ -38,17 +42,16 @@ CREATE TABLE "Flight" (
 );
 
 -- CreateTable
-CREATE TABLE "Attraction" (
+CREATE TABLE "Location" (
     "id" SERIAL NOT NULL,
-    "address" TEXT NOT NULL,
-    "city" TEXT NOT NULL,
-    "country" TEXT NOT NULL,
-    "description" TEXT,
-    "locationId" INTEGER NOT NULL,
+    "city" TEXT,
+    "cityName" TEXT NOT NULL,
+    "country" TEXT,
+    "countryName" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
-    CONSTRAINT "Attraction_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "Location_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
@@ -58,10 +61,11 @@ CREATE UNIQUE INDEX "Airport_airportId_key" ON "Airport"("airportId");
 ALTER TABLE "Airport" ADD CONSTRAINT "Airport_locationId_fkey" FOREIGN KEY ("locationId") REFERENCES "Location"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Flight" ADD CONSTRAINT "Flight_departureAirportId_fkey" FOREIGN KEY ("departureAirportId") REFERENCES "Airport"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "Attraction" ADD CONSTRAINT "Attraction_locationId_fkey" FOREIGN KEY ("locationId") REFERENCES "Location"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Flight" ADD CONSTRAINT "Flight_arrivalAirportId_fkey" FOREIGN KEY ("arrivalAirportId") REFERENCES "Airport"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Attraction" ADD CONSTRAINT "Attraction_locationId_fkey" FOREIGN KEY ("locationId") REFERENCES "Location"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "Flight" ADD CONSTRAINT "Flight_departureAirportId_fkey" FOREIGN KEY ("departureAirportId") REFERENCES "Airport"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
