f3cfd0ae3305f4edbda577872495334767c11337
farhan <farhanasfar21@gmail.com>
2026-01-13 18:35:48 +0600

    Add token to req the routes

---
+++ middlewares/token.js
@@ -0,0 +1,9 @@
+export const verifyToken = (req, res, next) => {
+  const token = req.headers.authorization?.split(' ')[1];
+  
+  if (token === process.env.API_TOKEN) {
+    next();
+  } else {
+    res.status(401).json({ error: "Invalid or missing token" });
+  }
+};
--- routes/travel.route.js
+++ routes/travel.route.js
@@ -1,14 +1,16 @@
 import { Router } from "express";
-import { locationSearch,detailsController } from "../controllers/locationSearch.controller.js";  
+import { locationSearch,detailsController } from "../controllers/locationSearch.controller.js";
+
+import { verifyToken } from "../middlewares/token.js";
 
 const travelRouter = Router();
 
 
 // defining routes
 // api/v1/travel/...
-travelRouter.route("/search/:locationname").get(locationSearch);
+travelRouter.route("/search/:locationname").get(verifyToken,locationSearch);
 
-travelRouter.route("/details/:id").get(detailsController);
+travelRouter.route("/details/:id").get(verifyToken,detailsController);
 
 
 export {travelRouter}
