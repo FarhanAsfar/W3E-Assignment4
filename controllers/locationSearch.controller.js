af702678d1217c87ec87c02e7507d71bc6628770
farhan <farhanasfar21@gmail.com>
2026-01-13 15:36:14 +0600

    Complete location search query

--- controllers/locationSearch.controller.js
+++ controllers/locationSearch.controller.js
@@ -3,7 +3,7 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import {ApiError} from "../utils/ApiError.js";
 import {ApiResponse} from "../utils/ApiResponse.js";
 
-const locationSearch = asyncHandler = async (req, res) => {
+const locationSearch = asyncHandler (async (req, res) => {
     const {locationname} = req.params;
 
     if(!locationname) {
@@ -57,7 +57,42 @@ const locationSearch = asyncHandler = async (req, res) => {
     if(!location) {
         throw new ApiError(404, "Location not found");
     }
-}
+
+    // storing geo info
+    const geoInfo = {
+        city: location.city,
+        cityName: location.cityName,
+        country: location.country,
+        countryName: location.countryName,
+    }
+
+    // storing flights info
+    const flightsInfo = new Map();
+    
+    location.airports.forEach(({ airportId, departureFlights, arrivalFlights }) => {
+      [...departureFlights, ...arrivalFlights].forEach(flight => {
+        flightsMap.set(flight.id, {
+          flightName: flight.flightName,
+          airlineName: flight.airlineName,
+          airportId,
+          city: location.cityName,
+          country: location.countryName,
+        });
+      });
+    });
+    const flights = Array.from(flightsInfo.values());
+
+    // storing attractions info
+    const attractionsInfo = location.attractions;
+
+    res.status(200).json(
+        new ApiResponse(200, "Location fetched successfully", {
+            geoInfo,
+            flights,
+            attractions: attractionsInfo,
+        })
+    );
+})
 
 
-export {location};
+export {locationSearch}
